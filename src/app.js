574b0e72a709ada30f3ac48357abc10dd168c418
Harsh <harshbit10@gmail.com>
2024-10-27 03:11:03 +0530

    app

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+// data can come from url, forms, json format, cookie, params.
+// use is used for middleware, configuration of the app.
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));  // public folder ( on our server) is accessible to the client.
+app.use(cookieParser());
+
+
+
+export default app;
+
+
+// express() is a function that creates an instance of an Express application.
+// This instance is used to configure the application and start the server.
+
+// request ---> baseUrl, body (forms, json format data), cookie, hostname, ip, params.
+// cookie ---> stored in the browser, can be accessed by the server.
+// response
--- src/db/index.js
+++ src/db/index.js
@@ -5,8 +5,6 @@ const connectDB = async() => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n Connected to MongoDB!! DB HOST : ${connectionInstance.connection.host} \n`);
-    
-    
     }catch(error){
         console.log("Error in connection  : ", error);
         throw error;
