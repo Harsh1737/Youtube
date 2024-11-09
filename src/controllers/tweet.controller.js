740c61ce13bdf3f0515594c4b92c73000d5a4c18
Harsh <harshbit10@gmail.com>
2024-11-09 12:25:23 +0530

    Implement tweet management features: create, retrieve, update, and delete tweets

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,18 +7,50 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content} = req.body
+
+    const user = await User.findById(req.user._id)
+    if (!user) throw new ApiError(404, "User not found")
+    
+    const tweet = new Tweet({
+        content,
+        owner: req.user._id
+    })
+    await tweet.save()
+    res.status(201).json(new ApiResponse(201, tweet))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId} = req.params
+    const user = await User.findById(userId)
+    if (!user) throw new ApiError(404, "User not found");
+
+    const tweets = await Tweet.find({owner: userId})
+    res.status(200).json(new ApiResponse(200, tweets))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const { tweetId } = req.params
+    const { content } = req.body
+    if (!content) throw new ApiError(400, "Content is required");
+    const tweet = await Tweet.findByIdAndUpdate(tweetId, {
+        $set: {content}
+    }, {new: true})
+    if (!tweet) throw new ApiError(404, "Tweet not found");
+
+    res.status(200).json(new ApiResponse(200, tweet))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    //TODO: delete tweet    
+    const { tweetId } = req.params
+    if(!isValidObjectId(tweetId)) throw new ApiError(400, "Invalid tweet ID");
+
+    const tweet = await Tweet.findByIdAndDelete(tweetId);
+    if (!tweet) throw new ApiError(404, "Tweet not found");
+    return res.json(new ApiResponse(204,"Tweet deleted successfully", tweet))
 })
 
 export {
