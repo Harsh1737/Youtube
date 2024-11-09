41a6b23842fd3762c342eda5049b800e2c689b12
Harsh <harshbit10@gmail.com>
2024-11-09 12:55:25 +0530

    like functionality added

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,22 +7,55 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if(!isValidObjectId(videoId)) throw new ApiError(400, "Invalid video id")
+    let like = await Like.findOne({video: videoId, likedBy: req.user?._id})
+    if(like){
+        await Like.findByIdAndDelete(like._id)
+        return res.status(201).json( new ApiResponse(201, "Like removed"))
+    }
+    await Like.create({video: videoId, likedBy: req.user._id})
+    return res
+    .status(201)
+    .json(new ApiResponse(201, "Video liked"))
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
-
+    if(!isValidObjectId(commentId)) throw new ApiError(400, "Invalid comment id")
+    let like = await Like.findOne({comment: commentId, likedBy: req.user?._id})
+    if(like){
+        await Like.findByIdAndDelete(like._id)
+        return res.status(201).json( new ApiResponse(201, "Like removed"))
+    }
+    await Like.create({comment: commentId, likedBy: req.user._id})
+    return res
+    .status(201)
+    .json(new ApiResponse(201, "Comment liked"))
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+    if(!isValidObjectId(tweetId)) throw new ApiError(400, "Invalid tweet id")
+    let like = await Like.findOne({tweet: tweetId, likedBy: req.user?._id})
+    
+    if (like){
+        await Like.findByIdAndDelete(like._id)
+        return res.status(201).json(new ApiResponse(201, "Like removed"))
+    }
+    await Like.create({tweet: tweetId, likedBy: req.user._id})
+    return res.status(201).json(new ApiResponse(201, "Tweet liked"))
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const userId = req.user?._id
+    if(!userId) throw new ApiError(401, "Unauthorized");
+    const likedVideos = await Like.find({likedBy: userId, video: {$exists: true}})
+    .populate("video")
+    .select("video")
+    return res.status(200).json(new ApiResponse(200, likedVideos))
 })
 
 export {
--- src/models/like.model.js
+++ src/models/like.model.js
@@ -5,21 +5,20 @@ const likeSchema = new Schema(
         video: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Video',
-            required: true,
         },
         comment: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Comment',
-            required: true,
         },
         tweet: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Tweet',
-            required: true,
         },
         likedBy: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',        },
+            ref: 'User',
+            required: true,
+        },
     },
     {
         timestamps: true,
